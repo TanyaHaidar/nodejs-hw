07e28e6502337471da17ec9c59195e0e3ba3c709
TanyaHaidar <tfilippenko1@gmail.com>
2025-11-06 20:22:08 +0200

    node2

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -3,7 +3,6 @@ import createHttpError from "http-errors";
 export const errorHandler = (err, req, res, next) => {
   if (err instanceof createHttpError.HttpError) {
     return res.status(err.status).json({
-      status: err.status,
       message: err.message,
     });
   }
@@ -11,7 +10,6 @@ export const errorHandler = (err, req, res, next) => {
   console.error("Unexpected error:", err);
 
   return res.status(500).json({
-    status: 500,
     message: "Internal Server Error",
   });
 };
