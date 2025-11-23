e4009b8a06e3700ecadfa1bfe349dd44671bc682
TanyaHaidar <tfilippenko1@gmail.com>
2025-11-23 15:43:36 +0200

    corrected

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { celebrate } from "celebrate";
 
 import {
   registerUser,
@@ -15,8 +14,8 @@ import {
 
 const router = Router();
 
-router.post("/register", celebrate(registerUserSchema), registerUser);
-router.post("/login", celebrate(loginUserSchema), loginUser);
+router.post("/register", registerUserSchema, registerUser);
+router.post("/login", loginUserSchema, loginUser);
 
 router.post("/refresh", refreshUserSession);
 router.post("/logout", logoutUser);
